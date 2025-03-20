68ebd0fa70d3d212bd122f04cdcccfac5464d6c1
Andrew C. Hopkins <Hopkins.AC@gmail.com>
2025-03-19 19:39:49 -0600

    SETUP - The main application project.

---
+++ Source/UI/Application/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿//
+// Copyright (C) 2025.  Andrew C. Hopkins.  All Rights Reserved.
+//
+
+using Orchid.UI.Infrastructure.Mvvm;
+
+namespace Orchid.UI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+   public string Greeting => "Welcome to Avalonia!";
+#pragma warning restore CA1822 // Mark members as static
+}
