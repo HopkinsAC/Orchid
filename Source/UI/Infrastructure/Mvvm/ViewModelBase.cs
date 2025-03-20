921fa364603c34ed529f1f67d4cfadc95518e8d8
Andrew C. Hopkins <Hopkins.AC@gmail.com>
2025-03-19 19:28:47 -0600

    SETUP - The UI infrastructure project.

---
+++ Source/UI/Infrastructure/Mvvm/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (C) 2025.  Andrew C. Hopkins.  All Rights Reserved.
+//
+
+namespace Orchid.UI.Infrastructure.Mvvm;
+
+public class ViewModelBase : BindableBase
+{
+   // Construction
+   //
+   
+   // Api
+   //
+   
+   // Implementation
+   //
+}
---
+++ Source/UI/Infrastructure/RegionNames.cs
@@ -0,0 +1,14 @@
+//
+// Copyright (C) 2025.  Andrew C. Hopkins.  All Rights Reserved.
+//
+
+namespace Orchid.UI.Infrastructure;
+
+public static class RegionNames
+{
+   /*
+   public const string PokeSearchRegion = "PokeSearchRegion";
+   public const string PokeNavigatorRegion = "PokeNavigatorRegion";
+   public const string PokeViewerRegion = "PokeViewerRegion";
+   */
+}
