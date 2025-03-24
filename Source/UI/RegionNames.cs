0105cddab4b1586043c77b169f9c0b1fddb87d83
Andrew C. Hopkins <Hopkins.AC@gmail.com>
2025-03-24 00:50:39 -0600

    CODE - Modified the main window for look and feel.

---
+++ Source/UI/RegionNames.cs
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2025.  Andrew C. Hopkins.  All Rights Reserved.
+//
+
+namespace Orchid.UI;
+
+public static class RegionNames
+{
+   public const string Books = "Books";
+   public const string BookShelf = "BookShelf";
+}
